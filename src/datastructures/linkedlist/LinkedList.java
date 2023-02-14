35a16a9b7d1c343ba6b1c0c8cc033131c428e0a2
Phil Fernandez <philvfernandez@gmail.com>
2023-02-13 17:34:04 -0700

    Section 21 - Coding exercise 1.

--- src/datastructures/linkedlist/LinkedList.java
+++ src/datastructures/linkedlist/LinkedList.java
@@ -16,6 +16,34 @@ public class LinkedList {
         length = 1;
     }
 
+    public void printList() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        if(head == null) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+    }
+
+    public void getTail() {
+        if(head == null) {
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Tail: " + tail.value);
+        }
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
     class Node {
         int value;
         Node next;
--- src/datastructures/linkedlist/Main.java
+++ src/datastructures/linkedlist/Main.java
@@ -4,5 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(4);
+
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        System.out.println("\nLinked List:");
+        myLinkedList.printList();
+
+        /*
+            Expected OUTPUT:
+            Head: 4
+            Tail: 4;
+            Length: 1
+
+            Linked List:
+            4
+         */
     }
 }

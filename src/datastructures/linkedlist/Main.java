dbb8a23dde4907a592a318a54f5778f94c5b2d40
Phil Fernandez <philvfernandez@gmail.com>
2023-02-14 14:52:23 -0700

    Section 32 - added reverse method.

--- src/datastructures/linkedlist/Main.java
+++ src/datastructures/linkedlist/Main.java
@@ -4,13 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         //Creates Link List with values of 2 and 1.
-        LinkedList myLinkedList = new LinkedList(11);
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
         myLinkedList.append(3);
-        myLinkedList.append(23);
-        myLinkedList.append(7);
+        myLinkedList.append(4);
 
-        myLinkedList.remove(2);
-
+        myLinkedList.reverse();
         myLinkedList.printList();
 
     }

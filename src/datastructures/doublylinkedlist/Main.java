76f0aee388ebbf583b5c361bd2c23bad5f22114c
Phil Fernandez <philvfernandez@gmail.com>
2023-02-14 20:32:40 -0700

    Section 39 - Added set method for doubly linked list.

--- src/datastructures/doublylinkedlist/Main.java
+++ src/datastructures/doublylinkedlist/Main.java
@@ -3,15 +3,12 @@ package datastructures.doublylinkedlist;
 public class Main {
     public static void main(String[] args) {
 
-        DoublyLinkedList myDLL = new DoublyLinkedList(0);
-        myDLL.append(1);
-        myDLL.append(2);
+        DoublyLinkedList myDLL = new DoublyLinkedList(11);
         myDLL.append(3);
+        myDLL.append(23);
+        myDLL.append(7);
 
-        //myDLL.printList();
-
-        System.out.println(myDLL.get(1).value);
-        System.out.println(myDLL.get(2).value + "\n");
+        myDLL.set(1, 4);
 
         myDLL.printList();
 

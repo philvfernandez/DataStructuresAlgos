d98047991161602394654ba4d7f0d3a27bdde59d
Phil Fernandez <philvfernandez@gmail.com>
2023-02-15 15:21:56 -0700

    Section 48 - Added enqueue method.

--- src/datastructures/queue/Main.java
+++ src/datastructures/queue/Main.java
@@ -2,10 +2,9 @@ package datastructures.queue;
 
 public class Main {
     public static void main(String[] args) {
-        Queue myQueue = new Queue(7);
+        Queue myQueue = new Queue(1);
 
-        myQueue.getFirst();
-        myQueue.getLast();
-        myQueue.getLength();
+        myQueue.enqueue(2);
+        myQueue.printQueue();
     }
 }
--- src/datastructures/queue/Queue.java
+++ src/datastructures/queue/Queue.java
@@ -12,6 +12,19 @@ public class Queue {
         length = 1;
     }
 
+    public void enqueue(int value) {
+        Node newNode = new Node(value);
+
+        if(length == 0) {
+            first = newNode;
+            last = newNode;
+        } else {
+            last.next = newNode;
+            last = newNode;
+        }
+        length++;
+    }
+
     public void printQueue() {
         Node temp = first;
         while (temp != null) {

90919e944d1a8511c967f23af8e23534f27c542c
Phil Fernandez <philvfernandez@gmail.com>
2023-02-15 10:49:03 -0700

    Section 45 - Added pop method.

--- src/datastructures/stack/Main.java
+++ src/datastructures/stack/Main.java
@@ -2,8 +2,12 @@ package datastructures.stack;
 
 public class Main {
     public static void main(String[] args) {
-        Stack myStack = new Stack(2);
-        myStack.push(1);
+        Stack myStack = new Stack(7);
+        myStack.push(23);
+        myStack.push(3);
+        myStack.push(11);
+
+        myStack.pop();
         myStack.printStack();
     }
 }

94c0970a8ddf36a1e2694e24b381cf53f71aceb5
Phil Fernandez <philvfernandez@gmail.com>
2023-02-15 10:39:46 -0700

    Section 43 - Added Stack, Node and Main classes.

---
+++ src/datastructures/stack/Stack.java
@@ -0,0 +1,37 @@
+package datastructures.stack;
+
+public class Stack {
+    private Node top;
+    private int height;
+
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getTop() {
+        System.out.println("Top: " + top.value);
+    }
+
+    public void getHeight() {
+        System.out.println("Height: " + height);
+    }
+
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+}

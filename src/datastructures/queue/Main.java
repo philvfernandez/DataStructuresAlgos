2c898275e8e46ab2550732b1b68f1e3b1c1f2ecd
Phil Fernandez <philvfernandez@gmail.com>
2023-02-15 15:13:04 -0700

    Section 47 - Added Queue class.

---
+++ src/datastructures/queue/Main.java
@@ -0,0 +1,11 @@
+package datastructures.queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue myQueue = new Queue(7);
+
+        myQueue.getFirst();
+        myQueue.getLast();
+        myQueue.getLength();
+    }
+}

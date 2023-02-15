c226c66e5aa7ed2afd140ff99a109420358e902c
Phil Fernandez <philvfernandez@gmail.com>
2023-02-15 15:31:22 -0700

    Section 49 - Added dequeue method.

--- src/datastructures/queue/Main.java
+++ src/datastructures/queue/Main.java
@@ -2,9 +2,17 @@ package datastructures.queue;
 
 public class Main {
     public static void main(String[] args) {
-        Queue myQueue = new Queue(1);
+        Queue myQueue = new Queue(2);
 
-        myQueue.enqueue(2);
-        myQueue.printQueue();
+        myQueue.enqueue(1);
+
+        //Returns Node with 2.
+        System.out.println(myQueue.dequeue().value);
+
+        //Returns Node with 1
+        System.out.println(myQueue.dequeue().value);
+
+        //Returns null
+        System.out.println(myQueue.dequeue());
     }
 }
--- src/datastructures/queue/Queue.java
+++ src/datastructures/queue/Queue.java
@@ -25,6 +25,22 @@ public class Queue {
         length++;
     }
 
+    public Node dequeue() {
+        if(length == 0) {
+            return null;
+        }
+        Node temp = first;
+        if(length == 1) {
+            first = null;
+            last = null;
+        } else {
+            first = first.next;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
+
     public void printQueue() {
         Node temp = first;
         while (temp != null) {

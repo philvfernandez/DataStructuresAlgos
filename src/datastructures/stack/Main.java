edd60d3140f97ded9b36649cf481224641d9d51e
Phil Fernandez <philvfernandez@gmail.com>
2023-02-15 10:43:57 -0700

    Section 44 - Added push method.

--- src/datastructures/stack/Main.java
+++ src/datastructures/stack/Main.java
@@ -2,11 +2,8 @@ package datastructures.stack;
 
 public class Main {
     public static void main(String[] args) {
-        Stack myStack = new Stack(4);
-
-        myStack.getTop();
-        myStack.getHeight();
-
+        Stack myStack = new Stack(2);
+        myStack.push(1);
         myStack.printStack();
     }
 }

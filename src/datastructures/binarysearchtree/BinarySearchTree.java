11afd96511a28ac65069ee8828fdf038c8dc3f45
Phil Fernandez <philvfernandez@gmail.com>
2023-02-15 17:04:04 -0700

    Section 55 - Added insert method.

--- src/datastructures/binarysearchtree/BinarySearchTree.java
+++ src/datastructures/binarysearchtree/BinarySearchTree.java
@@ -12,6 +12,46 @@ public class BinarySearchTree {
         root = null;
     } */
 
+    /*
+    Pseudo code:
+    create newNode
+    if root == null then root = new Node
+    temp = root
+    while loop
+      if newNode == temp return false -->> duplicate
+      if < left else > right
+      if null insert newNode else move to next
+     */
+    public boolean insert(int value) {
+        Node newNode = new Node(value); //create new node
+
+        if(root == null) { //empty tree
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+
+        while(true) {
+            if(newNode.value == temp.value) return false;
+
+            if(newNode.value < temp.value) { //go left if true
+                if(temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+
+            } else { //go right if true
+                if(temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+
+    }
+
     class Node {
         int value;
         Node left;

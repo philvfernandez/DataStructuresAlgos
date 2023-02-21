bdaba52387f78bfa71fb9c7ccdbfcc8575b15b33
Phil Fernandez <philvfernandez@gmail.com>
2023-02-21 11:25:10 -0700

    Section 74 - Added remove vertex method to Graph class.

--- src/datastructures/graph/Main.java
+++ src/datastructures/graph/Main.java
@@ -7,15 +7,17 @@ public class Main {
         myGraph.addVertex("A");
         myGraph.addVertex("B");
         myGraph.addVertex("C");
+        myGraph.addVertex("D");
 
         myGraph.addEdge("A", "B");
         myGraph.addEdge("A", "C");
-        myGraph.addEdge("B", "C");
+        myGraph.addEdge("A", "D");
+        myGraph.addEdge("B", "D");
+        myGraph.addEdge("C", "D");
 
         myGraph.printGraph();
 
-        myGraph.removeEdge("A", "B");
-
+        myGraph.removeVertex("D");
         myGraph.printGraph();
 
     }

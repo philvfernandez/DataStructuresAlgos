79ee4d70d221efd899f5d291142e4206e5e12bd4
Phil Fernandez <philvfernandez@gmail.com>
2023-02-21 15:35:28 -0700

    Section 86 - Added new BubbleSort clasa and method to implement bubble sort.

---
+++ src/datastructures/sorts/Main.java
@@ -0,0 +1,13 @@
+package datastructures.sorts;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] myArray = {4,2,6,5,1,3};
+        BubbleSort myBubbleSort = new BubbleSort();
+        myBubbleSort.bubbleSort(myArray);
+
+        System.out.println(Arrays.toString(myArray));
+    }
+}

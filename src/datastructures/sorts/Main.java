ac9b86ee9ecb2c5db1bb4bc39498f7149708e207
Phil Fernandez <philvfernandez@gmail.com>
2023-02-22 11:52:50 -0700

    Section 94 - Added new MergeSort class and merge method.

--- src/datastructures/sorts/Main.java
+++ src/datastructures/sorts/Main.java
@@ -10,10 +10,17 @@ public class Main {
         /* SelectSort mySelectionSort = new SelectSort();
         mySelectionSort.selectionSort(myArray); */
 
-        InsertionSort myInsertionSort = new InsertionSort();
+       /* InsertionSort myInsertionSort = new InsertionSort();
         myInsertionSort.insertionSort(myArray);
 
 
-        System.out.println(Arrays.toString(myArray));
+        System.out.println(Arrays.toString(myArray)); */
+
+        //Merge sort
+        int[] array1 = {1,3,7,8};
+        int[] array2 = {2,4,5,6};
+        MergeSort mergeSort = new MergeSort();
+
+        System.out.println(Arrays.toString(mergeSort.merge(array1,array2)));
     }
 }

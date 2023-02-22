8f366518a561d74521f08b302d6c7f50c8a0bd8e
Phil Fernandez <philvfernandez@gmail.com>
2023-02-22 14:52:42 -0700

    Section 106 - Added a Breadth First Search method to the BinarySearchTree class.

--- src/datastructures/binarysearchtree/BinarySearchTree.java
+++ src/datastructures/binarysearchtree/BinarySearchTree.java
@@ -1,5 +1,9 @@
 package datastructures.binarysearchtree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinarySearchTree {
     public Node root;
 
@@ -175,5 +179,25 @@ public class BinarySearchTree {
         }
 
     }
+
+    public ArrayList<Integer> BFS() {
+        Node currentNode = root;
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> results = new ArrayList<>();
+        queue.add(currentNode);
+
+        while (queue.size() > 0) {
+            currentNode = queue.remove();
+            results.add(currentNode.value);
+            if(currentNode.left != null) {
+                queue.add(currentNode.left);
+            }
+            if(currentNode.right != null) {
+                queue.add(currentNode.right);
+            }
+        }
+        return results;
+    }
+
 
 }
--- src/datastructures/binarysearchtree/Main.java
+++ src/datastructures/binarysearchtree/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         BinarySearchTree myBST = new BinarySearchTree();
 
-        myBST.insert(2);
+        /* myBST.insert(2);
         myBST.insert(1);
         myBST.insert(3);
 
@@ -12,15 +12,26 @@ public class Main {
         System.out.println("\nRoot->Left: " + myBST.root.left.value);
         System.out.println("\nRoot->Right: " + myBST.root.right.value);
 
-        myBST.deleteNode(2);
+        myBST.deleteNode(2); */
 
         /*
                3
               / \
              1  null
          */
-        System.out.println("\nRoot: " + myBST.root.value);
+        /* System.out.println("\nRoot: " + myBST.root.value);
         System.out.println("\nRoot->Left: " + myBST.root.left.value);
-        System.out.println("\nRoot->Right: " + myBST.root.right);
+        System.out.println("\nRoot->Right: " + myBST.root.right); */
+
+        //BFS
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(27);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        System.out.println(myBST.BFS());
     }
 }
